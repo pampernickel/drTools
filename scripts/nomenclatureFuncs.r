9fef7ff6a6188bab990deddf3cbeddf1615a43ea
Pam Dobay <maria.pamela.david@gmail.com>
2016-10-25 01:06:25 +0200

    Added function to find closest matching drug name

--- scripts/nomenclatureFuncs.r
+++ scripts/nomenclatureFuncs.r
@@ -1,6 +1,8 @@
 # set of functions for making names uniform
 # standard Format: data frame with samples on rows and drugs on columns
 
+require(stringr)
+
 '%ni%' <- Negate('%in%')
 
 format <- function(exp.res, attribute=c("logIC50", "logEC10", "logEC50", "logEC90",
@@ -31,4 +33,25 @@ formatDrugNames <- function(res.df, drug.list.all){
   drug.names.fin[which(is.na(drug.names.fin))] <- toupper(colnames(res.df)[which(is.na(drug.names.fin))])
   colnames(res.df) <- drug.names.fin
   return(res.df)
+}
+
+findClosestMatch <- function(drug.name, drug.list.all){
+  # input: character vector of drug names
+  # output: standard name of the drug, as indicated in the "final.name" column of drug.list.all
+  sapply(drug.name, function(x){
+    match <- NA
+    if (toupper(x) %in% drug.list.all$final.name){
+      toupper(x) -> match
+    } else if (length(grep(x, drug.list.all$all.names, ignore.case=T)) > 0) {
+      if (length(grep(x, drug.list.all$all.names, ignore.case=T)) == 1){
+        grep(x, drug.list.all$all.names, ignore.case=T) -> ind
+        drug.list.all$final.name[ind] -> match
+      } else {
+        # get closer match
+      }
+    } else {
+      # get closest match, but give a warning that it's second guessing
+    }
+  }) -> match
+  return(match)
 }
