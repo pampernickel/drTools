ce4bbabae84b123f8d29a11d1595cbea0e03822c
Pam Dobay <maria.pamela.david@gmail.com>
2016-11-16 17:06:10 +0100

    Misc. fixes

--- scripts/fitting.functions.R
+++ scripts/fitting.functions.R
@@ -100,8 +100,7 @@ getResponseClass <- function(y.dat, x.dat, curr.exp=NA){
     rbind(y.dat[1:(length(y.dat)-1)], y.dat[2:length(y.dat)]) -> pts
     slopes <- apply(pts, 2, 
                     function(x) lm(c(x[1],x[2])~c(1,2))$coefficients[2])
-    #mean(slopes)
-    #length(sapply(slopes, function(x) length(which(x < 0))))
+    
     if (lm(formula=y.dat~log10(x.dat), na.action = na.omit)$coefficients[2] < -0.07 &&
           mean(y.dat, na.rm=T) < 0.76 || mean(slopes) <= -0.1){ # || 
              #(length(which(is.na(y.dat[c((length(y.dat)-1),length(y.dat))])))==0 &&
