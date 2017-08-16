5c1f058b270a2de2267d1491a1f9331e421d7981
Pam Dobay <maria.pamela.david@gmail.com>
2017-08-16 19:44:25 +0200

    Created function to add a fit to an existing collection of fits; designed to be called for each derived parameter

--- scripts/dataCollectionFuncs.r
+++ scripts/dataCollectionFuncs.r
@@ -8,6 +8,25 @@
 # matrix, which contains the response data of the blood paper cohort
 #@...
 
-addFit <- function(){
+addFit <- function(assembled, res.df, patient.name=""){
+  # explicitly add patient name and check if has the LK code
+  if (length(grep("LK", patient.name, ignore.case = T) == 0)){
+    stop("Only primary patient data can be added to the ALL record. 
+         The name that you specified is inconsistent with patient information.")
+  }
   
+  .mapResponse(res.df, assembled, drug.list.all, patient.name) -> assembled.sub
+  if (getData ==T && patient.name %ni% rownames(assembled)){
+    # merge assembled, assembled.sub
+    assembled.sub[which(rownames(assembled.sub) %ni% rownames(assembled)),] -> pat
+    match(colnames(pat), colnames(assembled)) -> ind.match
+    ind.match[which(ind.match %ni% NA)] -> ind.match
+    rep(NA, ncol(assembled)) -> fin
+    fin[ind.match[which(ind.match %ni% NA)]] <- as.numeric(as.character(pat[1,2:ncol(pat)]))
+    rbind(assembled, fin) -> assembledNew
+    rownames(assembled) <- patient.name
+    save(assembledNew, file=paste("./r.data.files/", date(), ".rda", sep=""))
+  } if (getData ==T && pat.name %ni% rownames(assembled)){
+    warning("Patient record already exists in this file. No record added.")
+  }
 }
