5734e777eae31ee9a3ab4f75eb3e221e8038eb43
Pam Dobay <maria.pamela.david@gmail.com>
2017-08-16 15:47:47 +0200

    Precursor scripts for database functions

---
+++ scripts/dataCollectionFuncs.r
@@ -0,0 +1,13 @@
+# PDobay for AG Bourquin, KISPI
+# pamela.dobay@kispi.uzh.ch
+
+#@...
+# Routine that collects and saves drug response information from all runs
+# using patient material (i.e. with LK200x tag); would currently go for the
+# solution of storing in an r data file (per patient) and in the 20161012.heatmap.all.fits.rda
+# matrix, which contains the response data of the blood paper cohort
+#@...
+
+addFit <- function(){
+  
+}
