9d1675925bfadac478827ca4d4d49e06b3705282
Pam Dobay <maria.pamela.david@gmail.com>
2017-08-17 11:55:11 +0200

    Created .matchDrugs function from code within .mapResponse; currently works on averageRes() results, but will be adapted to handle character strings

--- scripts/dataFormattingFuncs.r
+++ scripts/dataFormattingFuncs.r
@@ -1,4 +1,52 @@
+.matchDrugs <- function(res.df){
+  # designed to match drugs created from a data frame generated with the drTools averageRes()
+  # call, can be expanded to deal with strings
+  res <- common.drugs <- unmatched.drugs <- NA
+  if (is.matrix(res.df) || is.data.frame(res.df)){
+    res.df[which(res.df >= 4.5)] <- 4.5
+    res.df[which(res.df <= min(assembled, na.rm=T))] <- min(assembled, na.rm=T)
+    toupper(colnames(res.df)) -> colnames(res.df)
+    toupper(colnames(assembled)) -> colnames(assembled)
+    
+    # cases where nothing was fitted
+    apply(res.df, 2, function(x) length(which(is.na(x)))) -> nas
+    as.numeric(which(nas == nrow(res.df))) -> ind
+    
+    if (length(ind) > 0){
+      # remove drugs where no fit was performed, then check
+      # again if the matrix has been converted to a named numeric
+      res.df[,-ind] -> res.df
+      nrow(res.df) -> e
+      as.matrix(res.df) -> conv
+      if (is.null(dim(res.df)) & ncol(conv) < nrow(conv)){
+        # case of named numeric vector
+        t(conv) -> res.df
+      }
+    }
+    
+    sapply(colnames(res.df), function(x) 
+      ifelse(length(grep(x, drug.list.all$all.names))==1, 
+             drug.list.all$final.name[grep(x, drug.list.all$all.names)],
+             x)) -> colnames(res.df)
+    intersect(colnames(res.df), colnames(assembled)) -> common.drugs
+    
+    if (length(grep("_",colnames(res.df))) > 0 && length(common.drugs) == 0){
+      # split & sub
+      gsub("\\.", "-", as.character(sapply(strsplit(colnames(res.df), "_"), 
+                                           function(x) x[1]))) -> colnames(res.df)
+      intersect(colnames(res.df), colnames(assembled)) -> common.drugs
+    }
+    
+    colnames(res.df)[which(colnames(res.df) %ni% common.drugs)] -> unmatched
+  }
+  
+  list(common.drugs, unmatched) -> res
+  names(res) <- c("common", "unmatched")
+  return(res)
+}
+
 .mapResponse <- function(res.df, assembled, drug.list.all=NULL, pat.name=NULL){
+  # conversion
   nrow(res.df) -> e
   as.matrix(res.df) -> conv
   if (is.null(dim(res.df)) & ncol(conv) < nrow(conv)){
@@ -6,40 +54,8 @@
     t(conv) -> res.df
   }
   
-  res.df[which(res.df >= 4.5)] <- 4.5
-  res.df[which(res.df <= min(assembled, na.rm=T))] <- min(assembled, na.rm=T)
-  toupper(colnames(res.df)) -> colnames(res.df)
-  toupper(colnames(assembled)) -> colnames(assembled)
-  
-  # cases where nothing was fitted
-  apply(res.df, 2, function(x) length(which(is.na(x)))) -> nas
-  as.numeric(which(nas == nrow(res.df))) -> ind
-  
-  if (length(ind) > 0){
-    # remove drugs where no fit was performed, then check
-    # again if the matrix has been converted to a named numeric
-    res.df[,-ind] -> res.df
-    nrow(res.df) -> e
-    as.matrix(res.df) -> conv
-    if (is.null(dim(res.df)) & ncol(conv) < nrow(conv)){
-      # case of named numeric vector
-      t(conv) -> res.df
-    }
-  }
-  
-  sapply(colnames(res.df), function(x) 
-    ifelse(length(grep(x, drug.list.all$all.names))==1, 
-           drug.list.all$final.name[grep(x, drug.list.all$all.names)],
-           x)) -> colnames(res.df)
-  intersect(colnames(res.df), colnames(assembled)) -> common.drugs
-  
-  if (length(grep("_",colnames(res.df))) > 0 && length(common.drugs) == 0){
-    # split & sub
-    gsub("\\.", "-", as.character(sapply(strsplit(colnames(res.df), "_"), 
-                                         function(x) x[1]))) -> colnames(res.df)
-    intersect(colnames(res.df), colnames(assembled)) -> common.drugs
-  }
-  
+  .matchDrugs(res.df) -> resm
+  resm$common -> common.drugs
   res.df[,which(colnames(res.df) %in% common.drugs)] -> res.df.sub
   assembled[,which(colnames(assembled) %in% common.drugs)] -> assembled.sub
   
