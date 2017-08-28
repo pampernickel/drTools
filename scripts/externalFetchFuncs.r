2032730a22025e3970227dcebcbddfe53065d66a
Pam Dobay <maria.pamela.david@gmail.com>
2017-08-28 23:02:55 +0200

    Changed default maxK, formatting

--- scripts/externalFetchFuncs.r
+++ scripts/externalFetchFuncs.r
@@ -12,22 +12,22 @@
 
 library(rclinicaltrials) #devtools
 
-getTrials <- function(query, otherTerms="", maxK=10){
+getTrials <- function(query, otherTerms="", maxK=50){
   # wrapper for .getTrials
   .getTrials(query, otherTerms, maxK) -> tids
   formatNCTIDs(tids) -> res
   return(res)
 }
 
-.getTrials <- function(query, otherTerms="", maxK=10){
+.getTrials <- function(query, otherTerms="", maxK=50){
   # given a query (a drug or a vector of drugs), return information on clinical trials, where available; 
   # in case the `otherTerms' parameter is not blank, add this option to the queries
-  sapply(query, function(x){
+  lapply(query, function(x){
     # note that clinicaltrials_search on a case where no results are available results in an
     # error; to handle this, the query will be restricted to the drug name and will be filtered
     # based on the other search terms at a later point
     y <- clinicaltrials_search(query = x)
-    tid <- NA
+    tid <- title <- NA
     if (nrow(y) > 0){
       #y <- clinicaltrials_download(query = paste(x, paste(otherTerms, collapse="+"), sep="+"), 
       #                          count = maxK, include_results = TRUE)  
@@ -41,13 +41,17 @@ getTrials <- function(query, otherTerms="", maxK=10){
         y[[1]][[1]][ind,] -> fin
         if (length(ind) > 0){
           tid <- fin$nct_id
+          title <- paste(substr(fin$brief_title, 1, 45), "...", sep="")
         } else {
           # return all results anyway
           tid <- y[[1]][[1]]$nct_id
+          title <- paste(substr(y[[1]][[1]]$brief_title, 1, 45), "...", sep="")
         }
       }
     }
-    return(tid)
+    list(tid, title) -> res
+    names(res) <- c("id", "title")
+    return(res)
   }) -> tids
   return(tids)
 }
@@ -55,8 +59,8 @@ getTrials <- function(query, otherTerms="", maxK=10){
 formatNCTIDs <- function(tids){
   # given some ids from clinicaltrials.gov
   sapply(tids, function(x)
-    paste(as.character(sapply(x, function(y)
-      paste("<a href=", paste("https://clinicaltrials.gov/ct2/show/", y, sep=""), ">", y, "<\a>", sep="")
+    paste(as.character(sapply(1:length(x$id), function(y)
+      paste("<a href=", paste("https://clinicaltrials.gov/ct2/show/", x$id[y], sep=""), ">", x$title[y], "<\\a><br>", sep="")
     )), collapse=",") -> ids
   ) -> ids
   return(ids)
